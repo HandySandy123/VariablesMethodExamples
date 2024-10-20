c607ce2e9bb8af5a261abf81f0cd89733cf2c9fa
HandySandy123 <sweetiechimi@duck.com>
2024-10-20 16:27:16 +0200

    Initial commit

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    public GameObject empty;
+
+    MyFirstVariableandMethod mMethod;
+    int number;
+    string value;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = gameObject.name;
+        mMethod = empty.gameObject.GetComponent<MyFirstVariableandMethod>();
+    }
+
+    public void OnClick()
+    {
+        //Debug.Log(gameObject.name);
+        bool success = int.TryParse(value, out number);
+        if (success)
+        {
+            mMethod.IncreaseAgeByX(number);
+        } else
+        {
+            Debug.Log("Input failed");
+        }
+    }
+}
---
+++ Assets/Scripts/MyFirstVariableandMethod.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MyFirstVariableandMethod : MonoBehaviour
+{
+    public int CurrentAge = 27;
+    public GameObject TextObject;
+
+    TMP_Text innerText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        innerText = TextObject.GetComponent<TMPro.TMP_Text>();
+        PrintAge();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            CurrentAge += 1;
+            PrintAge();
+        }
+    }
+
+    public void IncreaseAge()
+    {
+        CurrentAge += 1;
+        PrintAge();
+    }
+
+    public void IncreaseAgeByX(int X)
+    {
+        CurrentAge += X;
+        PrintAge();
+    }
+
+    void PrintAge()
+    {
+        innerText.text = "Current age is " + CurrentAge;
+        //Debug.Log(innerText.text);
+        //Debug.LogFormat("Current age is: {0}", CurrentAge);
+    }
+}
